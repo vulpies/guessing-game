b41020a975aaba4e8820a517756510cb3e43ec90
vulpies <anonim616@gmail.com>
2023-09-27 01:07:37 +0300

    tests are done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0
+        this.max = 0
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.answer = Math.round((this.max + this.min) / 2)
+        return this.answer
     }
 
     lower() {
-
+        this.max = this.answer;
     }
 
     greater() {
-
+        this.min = this.answer
     }
 }
 
